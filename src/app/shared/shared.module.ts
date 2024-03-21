960c4421f3d12afdcfed5837b6bdeb0e46808401
Max_Moses <maxmoses11@gmail.com>
2024-03-21 19:37:59 +0300

    added count-selector component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -5,6 +5,7 @@ import { ProductCardComponent } from './components/product-card/product-card.com
 import {RouterModule} from "@angular/router";
 import {FormsModule} from "@angular/forms";
 import { CategoryFilterComponent } from './components/category-filter/category-filter.component';
+import { CountSelectorComponent } from './components/count-selector/count-selector.component';
 
 
 
@@ -12,7 +13,8 @@ import { CategoryFilterComponent } from './components/category-filter/category-f
   declarations: [
     PasswordRepeatDirective,
     ProductCardComponent,
-    CategoryFilterComponent
+    CategoryFilterComponent,
+    CountSelectorComponent
   ],
   imports: [
     CommonModule,
@@ -22,7 +24,8 @@ import { CategoryFilterComponent } from './components/category-filter/category-f
   exports: [
     PasswordRepeatDirective,
     ProductCardComponent,
-    CategoryFilterComponent
+    CategoryFilterComponent,
+    CountSelectorComponent
   ]
 })
 export class SharedModule { }
