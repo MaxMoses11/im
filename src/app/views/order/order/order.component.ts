e327db8f406a9f0d397a585366c835675ffdec21
Max_Moses <maxmoses11@gmail.com>
2024-03-29 12:38:53 +0300

    continue order component

--- src/app/views/order/order/order.component.ts
+++ src/app/views/order/order/order.component.ts
@@ -1,4 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, OnInit, TemplateRef, ViewChild} from '@angular/core';
+import {CartService} from "../../../shared/services/cart.service";
+import {CartType} from "../../../../types/cart.type";
+import {DefaultResponseType} from "../../../../types/default-response.type";
+import {Router} from "@angular/router";
+import {MatSnackBar} from "@angular/material/snack-bar";
+import {DeliveryType} from "../../../../types/delivery.type";
+import {FormBuilder, Validators} from "@angular/forms";
+import {PaymentType} from "../../../../types/payment.type";
+import {MatDialog, MatDialogRef} from "@angular/material/dialog";
+import {OrderService} from "../../../shared/services/order.service";
+import {OrderType} from "../../../../types/order.type";
+import {HttpErrorResponse} from "@angular/common/http";
 
 @Component({
   selector: 'app-order',
@@ -7,9 +19,151 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OrderComponent implements OnInit {
 
-  constructor() { }
+  deliveryType: DeliveryType = DeliveryType.delivery;
+  cart: CartType | null = null;
+  totalAmount: number = 0;
+  totalCount: number = 0;
+  deliveryTypes = DeliveryType;
+  paymentTypes = PaymentType;
 
-  ngOnInit(): void {
+  orderForm = this.fb.group({
+    firstName: ['', Validators.required],
+    lastName: ['', Validators.required],
+    phone: ['', Validators.required],
+    fatherName: [''],
+    paymentType: [PaymentType.cashToCourier, Validators.required],
+    email: ['', [Validators.required, Validators.email]],
+    street: [''],
+    house: [''],
+    entrance: [''],
+    apartment: [''],
+    comment: [''],
+  });
+
+  @ViewChild('popup') popup!: TemplateRef<ElementRef>;
+  dialogRef: MatDialogRef<any> | null = null;
+
+  constructor(private cartService: CartService,
+              private orderService: OrderService,
+              private _snackBar: MatSnackBar,
+              private router: Router,
+              private fb: FormBuilder,
+              private dialog: MatDialog) {
+    this.updateDeliveryTypeValidation();
   }
 
+  ngOnInit(): void {
+    this.cartService.getCart()
+      .subscribe((data: CartType | DefaultResponseType) => {
+        if ((data as DefaultResponseType).error !== undefined) {
+          throw new Error((data as DefaultResponseType).message);
+        }
+
+        this.cart = (data as CartType);
+        if (!this.cart || (this.cart && this.cart.items.length === 0)) {
+          this.router.navigate(['/']).then();
+          this._snackBar.open('Корзина пустая')
+          return;
+        }
+        this.calculateTotal();
+      });
+  }
+
+  calculateTotal() {
+    this.totalAmount = 0;
+    this.totalCount = 0;
+    if (this.cart) {
+      this.cart.items.forEach(item => {
+        this.totalAmount += item.quantity * item.product.price;
+        this.totalCount += item.quantity;
+      });
+    }
+  }
+
+  changeDeliveryType(type: DeliveryType) {
+    this.deliveryType = type;
+    this.updateDeliveryTypeValidation();
+  }
+
+  updateDeliveryTypeValidation() {
+    if (this.deliveryType === DeliveryType.delivery) {
+      this.orderForm.get('street')?.setValidators(Validators.required);
+      this.orderForm.get('house')?.setValidators(Validators.required);
+    } else {
+      this.orderForm.get('street')?.removeValidators(Validators.required);
+      this.orderForm.get('house')?.removeValidators(Validators.required);
+      this.orderForm.get('street')?.setValue('');
+      this.orderForm.get('house')?.setValue('');
+      this.orderForm.get('entrance')?.setValue('');
+      this.orderForm.get('apartment')?.setValue('');
+    }
+
+    this.orderForm.get('street')?.updateValueAndValidity();
+    this.orderForm.get('house')?.updateValueAndValidity();
+  }
+
+  createOrder() {
+    if (this.orderForm.valid && this.orderForm.value.firstName && this.orderForm.value.lastName
+    && this.orderForm.value.phone && this.orderForm.value.paymentType && this.orderForm.value.email) {
+
+      const paramsObject: OrderType = {
+        deliveryType: this.deliveryType,
+        firstName: this.orderForm.value.firstName,
+        lastName: this.orderForm.value.lastName,
+        phone: this.orderForm.value.phone,
+        paymentType: this.orderForm.value.paymentType,
+        email: this.orderForm.value.email,
+      };
+
+      if (this.deliveryType === DeliveryType.delivery) {
+        if (this.orderForm.value.street) {
+          paramsObject.street = this.orderForm.value.street;
+        }
+        if (this.orderForm.value.apartment) {
+          paramsObject.apartment = this.orderForm.value.apartment;
+        }
+        if (this.orderForm.value.house) {
+          paramsObject.house = this.orderForm.value.house;
+        }
+        if (this.orderForm.value.entrance) {
+          paramsObject.entrance = this.orderForm.value.entrance;
+        }
+      }
+
+      if (this.orderForm.value.comment) {
+        paramsObject.comment = this.orderForm.value.comment;
+      }
+
+      this.orderService.createOrder(paramsObject)
+        .subscribe({
+          next: (data: OrderType | DefaultResponseType) => {
+            if ((data as DefaultResponseType).error !== undefined) {
+              throw new Error((data as DefaultResponseType).message);
+            }
+
+            this.dialogRef = this.dialog.open(this.popup);
+            this.dialogRef.backdropClick()
+              .subscribe(() => {
+                this.router.navigate(['/']).then();
+              });
+            this.cartService.setCount(0);
+          },
+          error: (errorResponse: HttpErrorResponse) => {
+            if (errorResponse.error && errorResponse.error.message) {
+              this._snackBar.open(errorResponse.error.message);
+            } else {
+              this._snackBar.open('Ошибка заказа');
+            }
+          }
+        })
+    } else {
+      this.orderForm.markAllAsTouched();
+      this._snackBar.open('Заполните необходимые поля');
+    }
+  }
+
+  closePopup() {
+    this.dialogRef?.close();
+    this.router.navigate(['/']).then();
+  }
 }
